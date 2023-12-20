55d57d925762d1776b9f35fc19493e14969c139c
mgusssh <146914392+mgusssh@users.noreply.github.com>
2023-12-20 10:28:03 +0500

    style and js (DOM)

---
+++ www/script.js
@@ -0,0 +1,28 @@
+let switchMode = document.getElementById("switchMode");
+
+switchMode.onclick = function () {
+    let theme = document.getElementById("theme");
+    
+    if (theme.getAttribute("href") == "css/light-index.css") {
+        theme.href = "css/dark-index.css";
+
+    } else {
+        theme.href = "css/light-index.css";
+    }
+}
+
+let likebtn = document.querySelector('#likebtn');
+let dislikebtn = document.querySelector('#dislikebtn');
+let input1 = document.querySelector('#input1');
+let input2 = document.querySelector('#input2');
+
+likebtn.addEventListener('click', ()=>{
+    input1.value = parseInt(input1.value) + 1;
+    input1.style.color = "#000000";
+})
+
+dislikebtn.addEventListener('click', ()=>{
+    input2.value = parseInt(input2.value) + 1;
+    input2.style.color = "#000000";
+})
+
